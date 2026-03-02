cf2677078a750d5c6dab6d088103709e85e288da
Dorine <dorine.vigier@mediaschool.me>
2026-03-02 12:47:25 +0100

    [STYLE] Page d'accueil

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center p-8">
+    </div>
+  )
+}
+
+export default App
