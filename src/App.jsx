a83e57c9ba67c80f70cba3313ea2620f934cc40f
Ewen_lp <124358937+EwenLP@users.noreply.github.com>
2026-03-16 09:21:02 +0100

    [FEAT] ajout du team builder avec sauvegarde et export PDF

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Pokedex from "./pages/Pokedex";
 import PokemonDetail from "./pages/PokemonDetail";
+import TeamBuilder from "./pages/TeamBuilder";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center p-8">
+			<div className="min-h-screen bg-slate-900 text-white p-8">
 				<Routes>
-				<Route path="/" element={<Pokedex />} />
-				<Route path="/pokedex" element={<Pokedex />} />
-				<Route path="/pokemon/:id" element={<PokemonDetail />} />
+					<Route path="/" element={<Pokedex />} />
+					<Route path="/pokedex" element={<Pokedex />} />
+					<Route path="/pokemon/:id" element={<PokemonDetail />} />
+					<Route path="/team" element={<TeamBuilder />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
