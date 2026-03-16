53e8fb7fa3679c71877201dfafdfa88ef9535eaa
Ewen_lp <124358937+EwenLP@users.noreply.github.com>
2026-03-16 12:04:21 +0100

    [FIX] ajustements parabranche

--- frontend/src/api/pokemonApi.js
+++ frontend/src/api/pokemonApi.js
@@ -10,3 +10,14 @@ export async function fetchPokemonDetails(url) {
   const res = await fetch(url);
   return res.json();
 }
+
+export async function fetchPokemonDescription(id) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+  const data = await res.json();
+
+  const entry = data.flavor_text_entries.find(
+      (e) => e.language.name === "en"
+  );
+
+  return entry?.flavor_text.replace(/\f/g, " ");
+}
