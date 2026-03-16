57f2be12b7d2ae7efdc789bde35ffb8dd1e29653
Dorine <dorine.vigier@mediaschool.me>
2026-03-16 15:28:12 +0100

    [REFACTOR] Optimisation API + séparation composant SearchBar

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import { typeTranslations } from "../utils/Types";
+
+export default function SearchBar({ pokemonList, setFilteredPokemon }) {
+	const [searchName, setSearchName] = useState("");
+	const [searchId, setSearchId] = useState("");
+	const [selectedType, setSelectedType] = useState("");
+	const [sort, setSort] = useState("id-asc");
+
+	useEffect(() => {
+
+		let filtered = [...pokemonList];
+
+		if (searchName) {
+			filtered = filtered.filter((pokemon) =>
+				pokemon.name.toLowerCase().includes(searchName.toLowerCase())
+			);
+		}
+
+		if (searchId) {
+			filtered = filtered.filter(
+				(pokemon) => pokemon.id === Number(searchId)
+			);
+		}
+
+		if (selectedType) {
+			filtered = filtered.filter((pokemon) =>
+				pokemon.types.includes(selectedType)
+			);
+		}
+
+		if (sort === "id-asc") {
+			filtered.sort((a, b) => a.id - b.id);
+		}
+
+		if (sort === "id-desc") {
+			filtered.sort((a, b) => b.id - a.id);
+		}
+
+		if (sort === "name-asc") {
+			filtered.sort((a, b) => a.name.localeCompare(b.name));
+		}
+
+		if (sort === "name-desc") {
+			filtered.sort((a, b) => b.name.localeCompare(a.name));
+		}
+
+		setFilteredPokemon(filtered);
+
+	}, [pokemonList, searchName, searchId, selectedType, sort, setFilteredPokemon]);
+
+	return (
+		<div className="flex flex-wrap gap-4 mb-6">
+
+		<input
+			type="text"
+			placeholder="Nom du Pokémon"
+			className="p-2 rounded border border-white text-white bg-slate-800"
+			value={searchName}
+			onChange={(e) => setSearchName(e.target.value)}
+		/>
+
+		<input
+			type="number"
+			placeholder="ID"
+			className="p-2 rounded border border-white text-white bg-slate-800 w-24"
+			value={searchId}
+			onChange={(e) => setSearchId(e.target.value)}
+		/>
+
+		<select
+			className="p-2 rounded border border-white text-white bg-slate-800"
+			value={selectedType}
+			onChange={(e) => setSelectedType(e.target.value)}
+		>
+			<option value="">Tous les types</option>
+
+			{Object.keys(typeTranslations).map((type) => (
+				<option key={type} value={type}>
+					{typeTranslations[type]}
+				</option>
+			))}
+		</select>
+
+		<select
+			className="p-2 rounded border border-white text-white bg-slate-800"
+			value={sort}
+			onChange={(e) => setSort(e.target.value)}
+		>
+			<option value="id-asc">ID ↑</option>
+			<option value="id-desc">ID ↓</option>
+			<option value="name-asc">Nom A → Z</option>
+			<option value="name-desc">Nom Z → A</option>
+		</select>
+
+		</div>
+	);
+}
--- frontend/src/pages/PokemonDetail.jsx
+++ frontend/src/pages/PokemonDetail.jsx
@@ -1,46 +1,39 @@
-import { useState, useEffect } from "react";
-import { fetchPokemonDescription } from "../api/pokemonApi";
 import TypeBadge from "../components/TypeBadge";
 
 export default function PokemonDetail({ pokemon }) {
-
-	const [description, setDescription] = useState("");
-
-	useEffect(() => {
-		if(!pokemon) return;
-		fetchPokemonDescription(pokemon.id).then(setDescription);
-	}, [pokemon]);
-
 	if (!pokemon) {
 		return (
-			<div className="flex items-center justify-center text-gray-400 gap-4 border border-sky-950/50 p-4">
-				<img className="object-scale-down h-6 w-6" src="/pokeball.png" alt="Pokeball" />
-				<p className="border-l border-l-sky-950/50 pl-4">Sélectionne un Pokémon</p>
-			</div>
+		<div className="flex items-center justify-center text-gray-400 gap-4 border border-sky-950/50 p-4">
+			<img className="object-scale-down h-6 w-6" src="/pokeball.png" alt="Pokeball" />
+			<p className="border-l border-l-sky-950/50 pl-4">Sélectionne un Pokémon</p>
+		</div>
 		);
 	}
 
 	return (
 		<div className="bg-slate-800 rounded-xl p-6">
-			<img
-				src={pokemon.image}
-				alt={pokemon.name}
-				className="mx-auto w-64"
-			/>
-			<div className="bg-slate-600 rounded-xl p-6">
-				<h2 className="text-2xl font-bold mt-4 capitalize flex justify-center items-center gap-4">
-					{pokemon.name}
-					<p className="text-base pt-2">No. {pokemon.id}</p>
-				</h2>
-				<div className="flex justify-center gap-2 mt-4">
-					{pokemon.types.map((type) => (
-						<TypeBadge key={type} type={type} />
-					))}
-				</div>
-				<p className="text-gray-300 mt-4 text-center">
-					{description}
-				</p>
+		<img
+			src={pokemon.image}
+			alt={pokemon.name}
+			className="mx-auto w-64"
+		/>
+
+		<div className="bg-slate-600 rounded-xl p-6">
+			<h2 className="text-2xl font-bold mt-4 capitalize flex justify-center items-center gap-4">
+			{pokemon.name}
+			<p className="text-base pt-2">No. {pokemon.id}</p>
+			</h2>
+
+			<div className="flex justify-center gap-2 mt-4">
+			{pokemon.types.map((type) => (
+				<TypeBadge key={type} type={type} />
+			))}
 			</div>
+
+			<p className="text-gray-300 mt-4 text-center">
+			{pokemon.description}
+			</p>
+		</div>
 		</div>
 	);
 }
